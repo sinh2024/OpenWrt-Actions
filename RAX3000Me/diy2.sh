46fae54f7afdf18d84534c5b6c859bec1f875a13
sinh2024 <101681405+sinh2024@users.noreply.github.com>
2025-04-05 14:48:13 +0800

    Update diy2.sh

--- RAX3000Me/diy2.sh
+++ RAX3000Me/diy2.sh
@@ -12,4 +12,4 @@
 # Modify default IP
 sed -i "s/192.168.1.1/192.168.5.1/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='ImmortalWrt'/hostname='RAX3000Me'/g" package/base-files/files/bin/config_generate
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='QiYueYi $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='sinh3543 $(date +"%y%m%d")'/g" package/base-files/files/etc/openwrt_release
