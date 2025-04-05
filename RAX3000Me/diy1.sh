ccafa84f91d060656fe1f46c085c4c1cd241f0ef
sinh2024 <101681405+sinh2024@users.noreply.github.com>
2025-04-05 14:47:45 +0800

    Update diy1.sh

--- RAX3000Me/diy1.sh
+++ RAX3000Me/diy1.sh
@@ -14,12 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
-# 自定义软件源
-git clone --depth 1 -b main https://github.com/QiYueYiya/openwrt-packages package/openwrt-packages
-# Mosdns
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/package-mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# Easytier
-git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
+
